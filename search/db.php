d8d8d63ace0508d99a5397dbaff8ed8f19d2370b
ahmedDoro <a.yusuf-doro@rgu.ac.uk>
2017-04-26 23:31:50 +0100

    Update db.php

--- search/db.php
+++ search/db.php
@@ -1,5 +1,5 @@
 <?php
 	
-  mysql_connect("localhost","id1474749_campusdate","") or die("error database");
-  mysql_select_db("id1474749_campusdate") or die("id1474749_campusdate");
+  mysql_connect("br-cdbr-azure-south-b.cloudapp.net","b5878316b539fe","8e5d969e") or die("error database");
+  mysql_select_db("campusdate") or die("campusdate");
 ?> 
